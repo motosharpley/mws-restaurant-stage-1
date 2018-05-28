646ca3a390505b0c21c53f5a0f68a27956093b53
motosharpley <motosharpley@gmail.com>
2018-05-27 19:02:12 -0500

    add alt tag/text rest list img

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let restaurants,
   neighborhoods,
   cuisines
-var map
+let map
 var markers = []
 
 /**
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name;
   li.append(image);
 
   const name = document.createElement('h1');
