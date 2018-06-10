6228ca323ad1de0e5de71437ad8edcca4d558b41
Bryan Sharpley <motosharpley@gmail.com>
2018-06-10 14:21:16 -0500

    try 443

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,10 +8,11 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    // const port = 5500 // Change this to your server port
+    const port = 443 // Change this to your server port
     // return `http://localhost:${port}/data/restaurants.json`;
     // return '/data/restaurants.json';
     return 'https://motosharpley.github.io/mws-stage-1/data/restaurants.json';
+    return `http://motosharpley.github.io/mws-stage-1:${port}/data/restaurants.json`;
   }
 
   /**
